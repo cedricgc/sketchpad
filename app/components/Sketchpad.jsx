7f6ec58599ab72d4dfb559434ba6e4cacac7dc34
Cedric Charly <cedric.charly@gmail.com>
2016-02-29 23:37:18 -0500

    More setup

--- app/components/Sketchpad.jsx
+++ app/components/Sketchpad.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Square from './Square'
 
-export default ({sketchpadSize, boxesPerSide}) => {
-  const squareSize = sketchpadSize / boxesPerSide;
-  const boxes = boxesPerSide * boxesPerSide;
+export default ({sketchpadSize, boxesPerSide, handlers}) => {
+  const squareSize = sketchpadSize / boxesPerSide
+  const boxes = boxesPerSide * boxesPerSide
 
   return (
     <div className="sketchpad" style={{width: sketchpadSize}}>
-      {Array(boxes).fill(<Square size={squareSize} />)}
+      {generateSquares(boxes, squareSize, handlers)}
     </div>
   )
 }
+
+function generateSquares(count, size, handlers) {
+  let squares = []
+  for(let i = 0; i < count; i++) {
+    squares.push(<Square key={i} size={size} {...handlers} />)
+  }
+  return squares
+}
+

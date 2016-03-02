ffba238cf0a1f268604ab94dfaa4c110693a3a32
Cedric Charly <cedric.charly@gmail.com>
2016-03-02 00:10:05 -0500

    Changed Squares to ES6 class

--- app/containers/Squares.jsx
+++ app/containers/Squares.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Square from '../components/Square'
+import handlers from '../lib/handlers'
 
-const Squares = ({count, sideLength, handlers}) => {
-  return (
-    <div>
-      {generateSquares(count, sideLength, handlers)}
-    </div>
-  )
+class Squares extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {count, sideLength, handers} = this.props
+    return (
+      <div>
+        {generateSquares(count, sideLength, handlers)}
+      </div>
+    )
+  }
 }
 
 function generateSquares(count, sideLength, handlers) {

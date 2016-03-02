ee42d086d9f8491c311b3e75b11f3d791f984dc0
Cedric Charly <cedric.charly@gmail.com>
2016-03-02 00:27:43 -0500

    created event handler stubs

---
+++ app/lib/handlers.js
@@ -0,0 +1,26 @@
+
+
+function defaultHandler(e) {
+  return null
+}
+
+function gradientHandler(e) {
+  return null
+}
+
+function trailHandler(e) {
+  return null
+}
+
+function randomHandler(e) {
+  return null
+}
+
+const handlers = {
+  defaultHandler,
+  gradientHandler,
+  trailHandler,
+  randomHandler
+}
+
+export default handlers
